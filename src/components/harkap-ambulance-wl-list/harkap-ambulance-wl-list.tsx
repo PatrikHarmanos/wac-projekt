f24829aa7fe2b5c04b72a072cddb95a76f33fd76
Ákos Kappel <kappelakos@gmail.com>
2024-04-30 14:27:08 +0200

    Fix case when zero devices found

--- src/components/harkap-ambulance-wl-list/harkap-ambulance-wl-list.tsx
+++ src/components/harkap-ambulance-wl-list/harkap-ambulance-wl-list.tsx
@@ -7,91 +7,59 @@ import { AmbulanceDeviceListApiFactory, DeviceListEntry } from '../../api/ambula
   shadow: true,
 })
 export class HarkapAmbulanceWlList {
-  @Event({ eventName: "entry-clicked" }) entryClicked: EventEmitter<string>;
+  @Event({ eventName: 'entry-clicked' }) entryClicked: EventEmitter<string>;
   @Prop() apiBase: string;
   @Prop() ambulanceId: string;
   @State() errorMessage: string;
 
-  waitingPatients: DeviceListEntry[];
+  devices: DeviceListEntry[];
 
-  private async getWaitingPatientsAsync(){
+  private async getDevicesAsync() {
     try {
-       const response = await
-         AmbulanceDeviceListApiFactory(undefined, this.apiBase).
-           getDeviceListEntries(this.ambulanceId)
-       if (response.status < 299) {
-         return response.data;
-       } else {
-         this.errorMessage = `Cannot retrieve list of devices: ${response.statusText}`
-       }
-     } catch (err: any) {
-       this.errorMessage = `Cannot retrieve list of devices: ${err.message || "unknown"}`
-     }
-     return [];
+      const response = await AmbulanceDeviceListApiFactory(undefined, this.apiBase).getDeviceListEntries(this.ambulanceId);
+      if (response.status < 299) {
+        return response.data;
+      } else {
+        this.errorMessage = `Cannot retrieve list of devices: ${response.statusText}`;
+      }
+    } catch (err: any) {
+      this.errorMessage = `Cannot retrieve list of devices: ${err.message || 'unknown'}`;
+    }
+    return [];
   }
 
-  // private async getWaitingPatientsAsync(){
-  //   return await Promise.resolve(
-  //     [{
-  //         name: 'Magnetická rezonancia',
-  //         section: 'Chirurgia',
-  //         since: new Date(Date.now() - 10 * 60).toISOString(),
-  //         estimatedStart: new Date(Date.now() + 65 * 60).toISOString(),
-  //         estimatedDurationMinutes: 15,
-  //         condition: 'Kontrola'
-  //     }, {
-  //         name: 'Röntgen',
-  //         section: 'Radiológia',
-  //         since: new Date(Date.now() - 30 * 60).toISOString(),
-  //         estimatedStart: new Date(Date.now() + 30 * 60).toISOString(),
-  //         estimatedDurationMinutes: 20,
-  //         condition: 'Teploty'
-  //     }, {
-  //         name: 'CRP prístroj 1',
-  //         section: 'Laboratórium',
-  //         since: new Date(Date.now() - 72 * 60).toISOString(),
-  //         estimatedStart: new Date(Date.now() + 5 * 60).toISOString(),
-  //         estimatedDurationMinutes: 15,
-  //         condition: 'Bolesti hrdla'
-  //     }]
-  //   );
-  // }
-
   async componentWillLoad() {
-    this.waitingPatients = await this.getWaitingPatientsAsync();
+    this.devices = await this.getDevicesAsync();
   }
 
   render() {
     return (
       <Host>
-        {this.errorMessage
-          ? <div class="error">{this.errorMessage}</div>
-          :
+        {this.errorMessage ? (
+          <div class="error">{this.errorMessage}</div>
+        ) : (
           <div class="container">
-            <h1>
-              Nemocnica HarKap - Zoznam zdravotníckeho vybavenia
-            </h1>
-            <md-elevated-button class="add-button" onclick={() => this.entryClicked.emit("@new")}>
+            <h1>Nemocnica HarKap - Zoznam zdravotníckeho vybavenia</h1>
+            <md-elevated-button class="add-button" onclick={() => this.entryClicked.emit('@new')}>
               <span>Pridať nové zariadenie</span>
             </md-elevated-button>
-            <md-list class="list">
-              {this.waitingPatients.map((device) =>
-                <md-list-item class="list-item" onClick={ () => this.entryClicked.emit(device.id)}>
-                  <div slot="headline">{device.name}</div>
-                  <div slot="supporting-text">{"Oddelenie: " + device.department.name}</div>
-                  <md-icon slot="start">devices</md-icon>
-                  <md-icon slot="end">chevron_right</md-icon>
-                </md-list-item>
-              )}
-            </md-list>
+            {this.devices ? (
+              <md-list class="list">
+                {this.devices?.map(device => (
+                  <md-list-item class="list-item" onClick={() => this.entryClicked.emit(device.id)}>
+                    <div slot="headline">{device.name}</div>
+                    <div slot="supporting-text">{'Oddelenie: ' + device.department.name}</div>
+                    <md-icon slot="start">devices</md-icon>
+                    <md-icon slot="end">chevron_right</md-icon>
+                  </md-list-item>
+                ))}
+              </md-list>
+            ) : (
+              <div>Nenašli sa žiadne zariadenia</div>
+            )}
           </div>
-        }
+        )}
       </Host>
     );
   }
-
-  // private isoDateToLocale(iso:string) {
-  //   if(!iso) return '';
-  //   return new Date(Date.parse(iso)).toLocaleTimeString()
-  // }
 }
